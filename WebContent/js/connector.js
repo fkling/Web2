0fba00bc045c17bc36b4b389045beeffdef361c2
Felix Kling <sprinterl@gmx.de>
2010-07-21 19:10:10 +0200

    making progress

--- WebContent/js/connector.js
+++ WebContent/js/connector.js
@@ -1,21 +1,103 @@
 var result = {
-	count: 5,
-	mails: {
-		1: {id: 1, from: 'Christina', subject: 'hello', content: 'nothing here', date: '12-03-2010', parent: null},
-		2: {id: 2, from: 'Felix', subject: 're: hello', content: 'nothing here too', date: '13-03-2010', parent: 1},
-		3: {id: 3, from: 'Arber', subject: 're: hello', content: 'nothing here...really?', date: '13-03-2010', parent: 1},
-		4: {id: 4, from: 'Christina', subject: 're: hello', content: 'really!', date: '14-03-2010', parent: 3},
-		5: {id: 5, from: 'Felix', subject: 'oh boy', content: 'this damn project', date: '17-03-2010', parent: null}
+	count : 5,
+	mails : {
+		1 : {
+			id : 1,
+			from : 'Christina',
+			subject : 'hello',
+			content : 'nothing here',
+			date : '2010-03-12',
+			parent : null
+		},
+		2 : {
+			id : 2,
+			from : 'Felix',
+			subject : 're: hello',
+			content : 'nothing here too',
+			date : '2010-03-13',
+			parent : 1
+		},
+		3 : {
+			id : 3,
+			from : 'Arber',
+			subject : 're: hello',
+			content : 'nothing here...really?',
+			date : '2010-03-13',
+			parent : 1
+		},
+		4 : {
+			id : 4,
+			from : 'Christina',
+			subject : 're: hello',
+			content : 'really!',
+			date : '2010-03-14',
+			parent : 3
+		},
+		5 : {
+			id : 5,
+			from : 'Felix',
+			subject : 'oh boy',
+			content : 'this damn project',
+			date : '2010-03-17',
+			parent : null
+		}
 	}
 };
 
+var result_thread = {
+	count : 4,
+	mails : {
+		1 : {
+			id : 1,
+			from : 'Christina',
+			subject : 'hello',
+			content : 'nothing here',
+			date : '2010-03-12',
+			children :[2, 3],
+			parentID: null
+		},
+		2 : {
+			id : 2,
+			from : 'Felix',
+			subject : 're: hello',
+			content : 'nothing here too',
+			date : '2010-03-13',
+			children : [],
+			parentID: 1
+		},
+		3 : {
+			id : 3,
+			from : 'Arber',
+			subject : 're: hello',
+			content : 'nothing here...really?',
+			date : '2010-03-13',
+			children: [4],
+			parentID: 1
+		},
+		4 : {
+			id : 4,
+			from : 'Christina',
+			subject : 're: hello',
+			content : 'really!',
+			date : '2010-03-14',
+			children: [],
+			parentID: 3
+		}
+	},
+	root : 1
+};
 
-function Connector() {};
+function Connector() {
+};
 
 Connector.prototype = {
-	searchForEmails: function(searchParams, callback) {
+	searchForEmails : function(searchParams, callback) {
 		log(searchParams);
 		callback(result);
+	},
+	
+	getThreadForMailId: function(mail_id, callback) {
+		callback(result_thread);
 	}
 };
 
--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,139 +1,125 @@
 
-
 var log = function(msg) {
-	if(console) {
-		log = function(msg) {console.log(msg);};
-	}
-	else {
-		log = function(msg){/*do nothing*/};
+	if (console) {
+		log = function(msg) {
+			console.log(msg);
+		};
+	} else {
+		log = function(msg) {/* do nothing */
+		};
 	}
 };
 
 var Application = {
-	init: function() {
-		$('#table-view').tablesorter()
-		this.createSearchField();
-		this.addSearchButton();
-		
-		$(document).delegate('#prev-date', 'click', function() {
-			var currentRow = Application.shadowTable.find('tr[rel='+ $('#facebox #msg-id').text() + ']');
-			log(currentRow);
-			Application.loadMsg(Application.lastResult.mails[currentRow.prev('tr').attr('rel')]);
-		});
-		$(document).delegate('#next-date', 'click', function() {
-			var currentRow = Application.shadowTable.find('tr[rel='+ $('#facebox #msg-id').text() + ']');
-			Application.loadMsg(Application.lastResult.mails[currentRow.next('tr').attr('rel')]);
-		});
+	threads: {},
+	init : function() {
+		$('#table-view table').tablesorter()
+		UIBuilder.initUI();
 	},
-	
-	createSearchField: function(node) {
-		
-		var fieldhtml = '<div class="search-option">';
-		fieldhtml += '<span class="add-button button">+</span><span class="remove-button button">-</span>';
-		
-		fieldhtml += '<span class="search-label">Search in </span>';
-		
-		fieldhtml += '<select class="field">';
-		fieldhtml += '<option value="from">From</option>';
-		fieldhtml += '<option value="subject">Subject</option>';
-		fieldhtml += '<option value="date">Date</option>';
-		fieldhtml += '<option value="content">Content</option>';
-		fieldhtml += '</select>';
-		
-		fieldhtml += '<span class="search-label"> for </span>';
-		fieldhtml += '<input type="text" class="search-value" value="" />';
-		
-		
-		fieldhtml += '</div>';
-		
-		var field = $(fieldhtml);
-		
-		
-		field.children('.add-button').click(function() {
-			$(this).siblings('.remove-button').show();
-			$(this).hide();
-			Application.createSearchField($(this).parent());
-		});
-		
-		field.children('.remove-button').click(function() {
-			if($('#search > .search-option').length == 2) {
-				$('#search > .search-option').not($(this).parent()).children('.remove-button').hide();
-			}
-			if($(this).parent().next('.search-option').length == 0) {
-				$(this).parent().prev('.search-option').children('.add-button').show();
-			}
-			$(this).parent().remove();
-		});
-		
-		if($('#search > .search-option').length == 0) {
-			field.children('.remove-button').hide();
-		}
-		
-		if(node) {
-			node.after(field);
-		}
-		else {
-			field.appendTo('#search');
+
+	serachAndDisplayEmails : function() {
+		var searchParams = {};
+
+		$('#search > .search-option').each(
+				function() {
+					searchParams[$('select', this).val()] = $('.search-value',
+							this).val();
+				});
+
+		gConnector
+				.searchForEmails(
+						searchParams,
+						function(result) {
+							Application.result = result;
+
+							if (result.count == 0) {
+								$('#result-msg').text("No mails found.");
+								return;
+
+							} else {
+								$('#result-msg').text(
+										"Found " + result.count + " mails:");
+
+								var sortedMails = Array();
+								for ( var id in result.mails) {
+									sortedMails.push(result.mails[id]);
+								}
+
+								Application.sortedMails = sortedMails;
+								Application.sortedMails.sort(function(a, b) {
+									return Date.parse(a.date).compareTo(
+											Date.parse(b.date));
+								});
+
+								Application.sortedMails
+										.forEach(function(mail, index, mails) {
+											mail.prevByDate = (index > 0) ? mails[index - 1]
+													: null;
+											mail.nextByDate = (index < mails.length - 1) ? mails[index + 1]
+													: null;
+										});
+
+								UIBuilder.displaySearchResult(result.mails);
+							}
+						});
+	},
+
+	getAndDisplayThreadForMail : function(mail) {
+		var t = $('#thread-view-' + mail.id);
+		if (t.length > 0) {
+			var rootId = t.closest('.thread-view-root').children('li').attr('rel');
+			UIBuilder.displayThreadFor(mail, Applications.threads[rootId][rootId], Applications.threads[rootId]);
+			return;
 		}
 
-	},
-	
-	addSearchButton: function() {
-		$('<button>Search!</button>')
-		.appendTo('#search')
-		.click(function() {
-			Application.serachAndDisplayEmails();
-		});
-	},
-	
-	serachAndDisplayEmails: function() {
-		var searchParams = {};
-		
-		$('#search > .search-option').each(function() {
-			searchParams[$('select', this).val()] = $('.search-value', this).val();
-		});
-		
-		gConnector.searchForEmails(searchParams, function(mails) { Application.displayEmails(mails);});
-		
-	},
-	
-	displayEmails: function(result) {
-		this.lastResult = result;
-		
-		$('#table-view > tbody').empty();
-		var rowhtml = Array();
-		for(var id in result.mails) {
-			var mail = result.mails[id];
-			rowhtml.push('<tr rel="' + id + '">');
-			for(var prop in {'subject':1, 'from':1, 'date':1}) {
-				rowhtml.push('<td>' + mail[prop] + '</td>');
-			}
-			rowhtml.push('</tr>');			
-		}
-		$(rowhtml.join('')).appendTo('#table-view > tbody');
-		
-		$('#table-view tbody tr').click(function() {
-			var msg_id = $(this).attr('rel');
-			var mail = Application.lastResult.mails[msg_id];
+		gConnector.getThreadForMailId(mail.id, function(result) {
+			UIBuilder.displayThreadFor(mail, result.mails[result.root], result.mails);
+			Application.threads[result.root] = result.mails;
+			var root = result.mails[result.root];
+			if(root.children.length > 0)
+				root.nextByThread = result.mails[root.children[0]];
 			
-			$.facebox({div: '#message-view'});
-			Application.loadMsg(mail);			
+			for(var i = 0, length = root.children.length;i < length;i++) {
+				setPrevNext(result.mails[root.children[i]], root, result.mails);
+			}
+			
+			function setPrevNext(mail, parent, mails) {
+				var siblings = parent.children;
+				var index = siblings.indexOf(mail.id);
+				mail.prevByThread = (index == 0) ? parent : mails[siblings[index-1]];
+				
+				if(mail.children.length > 0) {
+					mail.nextByThread = mails[mail.children[0]];
+				}
+				else {
+					mail.nextByThread = ancestor(mail, mails);
+				}
+				for(var i = 0, length = mail.children.length;i < length;i++) {
+					setPrevNext(mails[mail.children[i]], mail, mails);
+				}
+			}
+			
+			function ancestor(mail, mails) {
+				if (mail.parentID == null)
+					return null;
+
+				var parent = mails[mail.parentID];
+				var index = parent.children.indexOf(mail.id);
+				if (index < parent.children.length - 1) {
+					return mails[parent.children[index + 1]];
+				} else {
+					return ancestor(parent, mails);
+				}
+			}
+			
+			
 		});
-		$('#table-view tbody tr:even').addClass('alt');
-		
-		$("#table-view").trigger("update"); 
-        $("#table-view").show();
-        
-        this.shadowTable = $('#table-view').clone(false).appendTo('#meta').tablesorter();
-        this.shadowTable.trigger('sorton', [[[2,0]]]);
 	},
-	loadMsg: function(mail) {
-		if(mail) {
-			$('#facebox #msg-id').text(mail.id);
-			$('#facebox #msg-sender').text(mail.from);
-			$('#facebox #msg-subject').text(mail.subject);
-			$('#facebox #msg-date').text(mail.date);
-			$('#facebox #msg-content p').text(mail.content);
-		}
+
+	getNextByThread : function(mail) {
+		UIBuilder.showMsg(mail.nextByThread);
+	},
+	getPrevByThread : function(mail) {
+		UIBuilder.showMsg(mail.prevByThread);
 	}
 };
